1c6de09d590de06157fd7b035e858fc3a891fd7c
nfandre <andrelive.05@hotmail.com>
2019-09-01 13:55:22 -0300

    aula 04 modulos

---
+++ Aula04/calc.js
@@ -0,0 +1,13 @@
+var somaF = require('./soma');
+var subF = require('./subtrair');
+var multF = require('./mult');
+var divF = require('./dividir');
+
+var a = 10;
+var b = 10;
+
+
+console.log(somaF(a,b));
+console.log(subF(a,b));
+console.log(multF(a,b));
+console.log(divF(a,b));
