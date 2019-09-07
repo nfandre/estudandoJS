e20cb262679d7e72905c954d15ae3eef634a5f0b
nfandre <andrelive.05@hotmail.com>
2019-09-07 16:32:30 -0300

    Aula20 - Como criar um formulário

--- Aula19 - handlebars/index.js
+++ Aula19 - handlebars/index.js
@@ -1,18 +1,24 @@
 const express = require("../expressModulo/node_modules/express");
 const app = express();
-const handlebars = require("../expressModulo/node_modules/express-handlebars")
+const handlebars = require('../expressModulo/node_modules/express-handlebars')
 const Sequelize = require('../expressModulo/node_modules/sequelize');
 
 //Config
   //Template Engine
-    app.engine('handlebars', handlebars({defaultlayot: 'main'}))
-    app.set('view enine', 'handlebars')
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
   //Conexão com banco de dados mysql
   const sequelize = new Sequelize('test','root','415263',{
     host:"localhost",
     dialect: 'mysql'
-  } )
+  })
 
+//Rotas
+app.get('/cadastro', function(req, res){
+    res.render('formulario')
+    //res.send("teste")
+})
 
 app.listen(8082, function(){
   console.log("Servidor rodando na url http://localhost:8082/");
