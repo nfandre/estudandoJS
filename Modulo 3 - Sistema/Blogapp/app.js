0e70c0e7e1e15b77ce069bcec184fa8f7682a946
nfandre <andrelive.05@hotmail.com>
2019-09-09 12:06:44 -0300

    Aula39 - configurar sessões

--- Modulo 3 - Sistema/Blogapp/app.js
+++ Modulo 3 - Sistema/Blogapp/app.js
@@ -6,7 +6,22 @@
     const admin = require("./routes/admin") 
     const path = require('path')
     const mongoose = require('mongoose')
+    const session = require("express-session")
+    const flash = require("connect-flash")
 //Configurações
+    //sessão
+        app.use(session({
+            secret: "qualquer coiisa",
+            resave: true,
+            saveUninitialized: true
+        }))    
+        app.use(flash())
+    //middlewares
+        app.use((req, res, next) =>{
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
