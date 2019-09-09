5f7314f79a1ae85b0d91ab4938bad8caf5323ebd
nfandre <andrelive.05@hotmail.com>
2019-09-09 12:33:54 -0300

    Como validar formulários no Express

--- Modulo 3 - Sistema/Blogapp/routes/admin.js
+++ Modulo 3 - Sistema/Blogapp/routes/admin.js
@@ -21,16 +21,35 @@ router.get('/categorias/add', (req, res) =>{
 })
 
 router.post('/categorias/nova', (req,res) =>{
-    console.log( req.body.nome, req.body.slug)
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+    var erros = []
+
+    if(!req.body.nome ||  typeof req.body.nome == undefined || req.body.nome ==null){
+        erros.push({texto: "Nome inválido"})
+    }
+    if(!req.body.slug ||  typeof req.body.slug == undefined || req.body.slug ==null){
+        erros.push({texto: "Slug inválido"})
     }
 
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log("Cadastro efetuado")
-    }).catch((erro)=>{
-        console.log("erro " + erro)
-    })
+    if(req.body.nome.length <2){
+        erros.push({texto: "Nome da categoria muito pequeno"})
+    }
+
+    if(erros.length >0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(()=>{
+            req.flash("success_msg", "Categoria cadastrada com sucesso")
+            res.redirect("/admin/categorias")
+        }).catch((erro)=>{
+            req.flash("error_msg", "Houve um erro ao salvar a categoria. Tente novametne")
+            console.log("erro " + erro)
+            res.redirect("/admin")
+        })
+    }
 })
 module.exports = router
