ded214331fa8de94561d093578ae1e95b48f14f3
nfandre <andrelive.05@hotmail.com>
2019-09-09 09:35:45 -0300

    Aula33 - arquivos estáticos

--- Modulo 3 - Sistema/Blogapp/app.js
+++ Modulo 3 - Sistema/Blogapp/app.js
@@ -3,6 +3,8 @@
     const handlebars = require('express-handlebars')
     const bodyParser = require("body-parser")
     const app = express()
+    const admin = require("./routes/admin")
+    const path = require('path')
     //const mongoose = require("mongoose")
 //Configurações
     //Body Parser
@@ -12,8 +14,12 @@
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
     //Mongoose
-//Rotas
+        //em breve
+    //Public
+        app.use(express.static(path.join(__dirname, "public")))
 
+//Rotas
+    app.use('/admin', admin)
 
 //Outros
 const PORT = 8081
