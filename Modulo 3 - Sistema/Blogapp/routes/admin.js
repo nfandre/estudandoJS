0d3e7653ce280722f02e1163dcdb5da4c4cbdcec
nfandre <andrelive.05@hotmail.com>
2019-09-09 10:55:04 -0300

    Aula36 - Cadastrando categorias no banco de dados

--- Modulo 3 - Sistema/Blogapp/routes/admin.js
+++ Modulo 3 - Sistema/Blogapp/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -15,5 +18,19 @@ router.get('/categorias', (req, res) =>{
 
 router.get('/categorias/add', (req, res) =>{
     res.render("admin/addcategorias")
+})
+
+router.post('/categorias/nova', (req,res) =>{
+    console.log( req.body.nome, req.body.slug)
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log("Cadastro efetuado")
+    }).catch((erro)=>{
+        console.log("erro " + erro)
+    })
 })
 module.exports = router
