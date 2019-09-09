570cb63f74c2ebd957f88b3dfc94416500e4b116
nfandre <andrelive.05@hotmail.com>
2019-09-09 14:16:15 -0300

    Aula 42 - Editando categoria

--- Modulo 3 - Sistema/Blogapp/routes/admin.js
+++ Modulo 3 - Sistema/Blogapp/routes/admin.js
@@ -58,4 +58,34 @@ router.post('/categorias/nova', (req,res) =>{
         })
     }
 })
+
+router.get("/categorias/edit/:id", (req,res)=>{
+    Categoria.findOne({_id: req.params.id}).then((categoria)=>{
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err)=>{
+        req.flash("error_msg", "Esta categoria não existe")
+        res.redirect("/admin/categorias")
+    })
+  
+
+})
+
+router.post("/categorias/edit", (req,res)=>{
+    Categoria.findOne({_id: req.body.id}).then((categoria)=>{
+        
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(()=>{
+            req.flash("success_msg", "categoria edita com sucesso")
+            res.redirect("/admin/categorias")
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria")
+        })
+
+    }).catch((err)=>{
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect("/admin/categorias")
+    })
+})
 module.exports = router
